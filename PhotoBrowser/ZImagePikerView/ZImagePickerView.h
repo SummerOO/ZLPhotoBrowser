658125fd338176b6575bbc20722d4597b30f603b
zr <zhangru_zhang@163.com>
2018-10-17 18:02:44 +0800

    测试

---
+++ PhotoBrowser/ZImagePikerView/ZImagePickerView.h
@@ -0,0 +1,17 @@
+//
+//  ZImagePickerView.h
+//  ZLPhotoBrowser
+//
+//  Created by Chris on 2018/10/17.
+//  Copyright © 2018年 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZImagePickerView : BaseView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PhotoBrowser/ZImagePikerView/ZImagePickerView.m
@@ -0,0 +1,13 @@
+//
+//  ZImagePickerView.m
+//  ZLPhotoBrowser
+//
+//  Created by Chris on 2018/10/17.
+//  Copyright © 2018年 long. All rights reserved.
+//
+
+#import "ZImagePickerView.h"
+
+@implementation ZImagePickerView
+
+@end
